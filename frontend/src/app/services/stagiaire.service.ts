7467c60485f92799e334ad726c93d8662b8f0afd
Hakim Salah <hakim.salah@ensi-uma.tn>
2025-09-29 21:01:02 +0100

    conf url

--- frontend/src/app/services/stagiaire.service.ts
+++ frontend/src/app/services/stagiaire.service.ts
@@ -7,16 +7,17 @@ import { Observable } from 'rxjs';
   providedIn: 'root',
 })
 export class StagiaireService {
-  baseUrl = 'http://backend-service:9090/api/stagiaires';
+  // ✅ URL RELATIVE - sera proxyfiée par Nginx
+  baseUrl = '/api/stagiaires';
 
   constructor(private http: HttpClient) {}
 
   getStagiaires(): Observable<Stagiaire[]> {
-    return this.http.get<Stagiaire[]>(`${this.baseUrl}/`);
+    return this.http.get<Stagiaire[]>(this.baseUrl);
   }
 
   postStagiaire(stagiaire: Stagiaire): Observable<Stagiaire> {
-    return this.http.post<Stagiaire>(`${this.baseUrl}/`, stagiaire);
+    return this.http.post<Stagiaire>(this.baseUrl, stagiaire);
   }
 
   deleteStagiaire(id: number): Observable<void> {
