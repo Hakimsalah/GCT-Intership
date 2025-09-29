dcad9f456055ee55b5575e3ad55822a72792515e
Hakim Salah <hakim.salah@ensi-uma.tn>
2025-09-29 21:21:44 +0100

    demo app

--- backend/src/main/java/com/example/demo/DemoApplication.java
+++ backend/src/main/java/com/example/demo/DemoApplication.java
@@ -2,12 +2,18 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+    "com.example.demo",
+    "com.example.demo.Services",      // Scanner explicitement
+    "com.example.demo.Repository",    // Scanner explicitement
+    "com.example.demo.controller",
+    "com.example.demo.model"
+})
 public class DemoApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 }
